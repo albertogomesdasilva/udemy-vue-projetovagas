7c951aab9cf6e790ee8a26ee1a5c41e1de962922
Alberto Gomes da Silva <albertogomesdasilva@homtmail.com>
2022-11-12 23:27:31 -0300

    projeto vujs

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Componente App</h1>
+   
+    <topoPadrao />
+    <conteudo></conteudo>
+  </div>
+</template>
+
+<script>
+import TopoPadrao from '@/components/layouts/TopoPadrao.vue'
+import Conteudo from '@/components/layouts/Conteudo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Conteudo,
+    TopoPadrao
+  }
+}
+</script>
+
+<style scoped>
+  p {
+    color: red
+  }
+</style>
---
+++ src/components/layouts/Conteudo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+   <h1>{{ titulo }}</h1>
+   <!-- <button @click="atualizarConteudo">Atualizar</button> -->
+   <button @click="conteudo = 'home'">Home</button>
+   <button @click="conteudo = 'publicar-vaga'">Publicar Vaga</button>
+  
+    <!-- <home></home>
+    <publicar-vaga></publicar-vaga> -->
+    <keep-alive>
+
+        <component :is="conteudo" />
+    </keep-alive>
+</div>
+</template>
+
+<script>
+    import Home from '@/components/views/Home.vue'
+    import PublicarVaga from '@/components/views/PublicarVaga.vue';
+    export default {
+        name: 'Conteudo',
+
+        components: {
+            Home,
+            PublicarVaga
+        }, 
+        data: () => ({
+            titulo: 'Componente Conteúdo',
+            conteudo: 'home'
+        }),
+        methods: {
+            atualizarConteudo() {
+                this.titulo += '*'
+            }
+        }
+    }
+
+
+</script>
+
+<style scoped>
+  
+</style>
+
+
